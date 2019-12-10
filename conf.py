3a0753d7c9784d288241de8907554728706c4332
miki-kenga <mikikenga@gmail.com>
2019-12-10 18:54:22 +0900

    conf.pyとcssを修正しました

--- conf.py
+++ conf.py
@@ -17,7 +17,7 @@ import time
 
 # Data about this site
 BLOG_AUTHOR = "kenken"  # (translatable)
-BLOG_TITLE = "cd sitename"  # (translatable)
+BLOG_TITLE = "けんけんのブログ"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
 SITE_URL = "https://github.com/kenga-miki/mikikenga.git/"
